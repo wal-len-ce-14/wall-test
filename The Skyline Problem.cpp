854e7ae9279ade2661ac4a545e1993d1669e0e8e
wal-len-ce-14 <a33369097@gmail.com>
2022-03-29 22:44:50 +0800

    correct pointer

--- The Skyline Problem.cpp
+++ The Skyline Problem.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int **getSkyline(int**);
 
 int main(){
-	int count, n, i = 0, j = 0, buildings[10][3] = {-1};  /// [left.i, right.i, height.i]
+	int count, n, i = 0, j = 0, buildings[20][3] = {-1};  /// [left.i, right.i, height.i]
 	cout << "How many buildings are there? ";
 	cin >> count;
 	cout << "Enter the buildings: ";
@@ -15,8 +15,10 @@ int main(){
 		}
 		j++;
 	}
+	cout << "sd";
 	int *a = buildings[0];
-	int** skyline = getSkyline(&a);
+	int* sky = getSkyline(&a)[0];
+	int** skyline = &sky;
 	n = j;
 	for(j = 0; j < n; j++){
 		for(i = 0; i < 2; i++){
@@ -28,39 +30,47 @@ int main(){
 }
 
 int **getSkyline(int** buildings){
-	int i, j = 0, k = 0, n[20][2] = {0}, lay = 0, max_H = 0;
-	for(i = 0; i < 1000; i++){   
-		cout << "10101\n";
-		if(buildings[j][2] < 0){
-			break;
-		}
-		if(i == buildings[j][0]){
-			if(buildings[j][2] > max_H){
-				n[k][0] = buildings[j][0];
-				n[k][1] = buildings[j][2];
-				max_H = buildings[j][2];
-				lay = j;
-				j++;
-				k++;
-			}else if(buildings[j][2] < max_H && buildings[j][1] > buildings[lay][1]){
-				n[k][0] = buildings[lay][1];
-				n[k][1] = buildings[j][2];
-				max_H = buildings[j][2];
-				lay = j;
-				j++;
-				k++;
-				if(buildings[j][0] > buildings[lay][1]){
-					n[k][0] = buildings[lay][1];
-					n[k][1] = 0;
-					max_H = 0;
-					lay = 0;
-					k++;
-				}
-			}else j++;
-		}		
+	int i = 0, j, buil = 0, lay, max_H = 0, sky[20][2] = {0}, x = 0, tem;
+	while(true){
+        if(x == buildings[buil][0] && max_H < buildings[buil][2]){
+            sky[i][0] = x;
+            sky[i][1] = buildings[buil][2];
+            max_H = buildings[buil][2];
+            lay = buil;
+            buil++;
+            i++;
+        }else if(x == buildings[lay][1]){
+            max_H = 0;
+            for(j = 0; j < 20, buildings[j][0] < buildings[lay][1], buildings[j][2] > 0; j++){
+                if(buildings[j][1] > buildings[lay][1] && max_H < buildings[j][2]){
+                    max_H = buildings[j][2];
+                    tem = j;
+                }
+            }
+            if(max_H != 0){
+                sky[i][0] = x;
+                sky[i][1] = max_H;
+                lay = tem;
+                i++;
+                buil = lay+1;
+            }else{
+                sky[i][0] = x;
+                sky[i][1] = max_H;
+                i++;
+                buil = j;
+            }
+        }
+
+        if(buildings[buil][2] < 0){
+            break;
+        }
+
+        x++;
 	}
-	int *v = n[0]; 
-	return &v;
+
+
+	int *s = sky[0];
+	return &s;
 }
 
 
